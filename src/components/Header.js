21203e1dff591d976f7a4b264425fb0d978e9178
HoracioGutierrez <horacio.estevez@gmail.com>
2022-07-19 14:46:25 -0300

    Desafio 2 : JSX

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <header id="header">
+            <h1 className="titulo" >Titlo</h1>
+            <nav>
+                <a href="#">link</a>
+                <a href="#">link</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
